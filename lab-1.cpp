7fab715d54e535d155c61925c9971d7ac0e37da2
begibistro <artemegerevschool@yandex.ru>
2021-02-26 22:56:36 +0300

    Доработал

--- lab-1.cpp
+++ lab-1.cpp
@@ -4,44 +4,56 @@
 #include <math.h>
 using namespace std;
 
+float Disc(float a, float b, float c) {
+	return (b * b) - (4 * a * c);
+}
+
+float Qudar(float a, float b, float c) {
+
+	if (a == 0 && b != 0) {
+		cout << "Root: " << -c / b << endl;
+		return 0;
+	}
+	else if (a == 0 && b == 0) {
+		if (c == 0) {
+			cout << "The equation has an infinite number of roots" << endl;
+			return 0;
+		}
+		else {
+			cout << "No roots" << endl;
+			return 0;
+		}
+	}
+	else {
+
+		float D = Disc(a, b, c);
+
+		if (D < 0) {
+			cout << "Discriminant< 0, no valid roots" << endl;
+			return 0;
+		}
+
+		else if (D == 0) {
+			cout << "Root: " << -b / (2 * a) << endl;
+			return 0;
+		}
+		else {
+			cout << "The equation has a two roots: " << (-b + sqrt(D)) / (2 * a) << " and " << (-b - sqrt(D)) / (2 * a) << endl;
+			return 0;
+		}
+	}
+}
+
 int main() {
 	float a, b, c, D;
 
-		cout << "Enter coefficents a, b, c of ax^2 + bx + c = 0 through space" << endl;
-		cin >> a;
-		cin >> b;
-		cin >> c;
+	cout << "Enter coefficents a, b, c of ax^2 + bx + c = 0 through space" << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
 
-		if (a == 0 && b != 0) {
-			cout << "Root: " << -c / b << endl;
-			return 0;
-		}
-		else if (a == 0 && b == 0) {
-			if (c == 0) {
-				cout << "The equation has an infinite number of roots" << endl;
-				return 0;
-			}
-			else {
-				cout << "No roots" << endl;
-				return 0;
-			}
-		}
-		else {
+	Qudar(a, b, c);
+	
 
-			D = (b * b) - (4 * a * c);
-
-			if (D < 0) {
-				cout << "Discriminant< 0, no valid roots" << endl;
-				return 0;
-			}
-
-			else if (D == 0) {
-				cout << "Root: " << -b / (2 * a) << endl;
-				return 0;
-			}
-			else {
-				cout << "The equation has a two roots: " << (-b + sqrt(D)) / (2 * a) << " and " << (-b - sqrt(D)) / (2 * a) << endl;
-				return 0;
-			}
-		}
+	return 0;
 }
